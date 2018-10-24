49afa604365b654bb510353947778e584dbb5ce9
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-24 00:36:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector)  {
+ return document.querySelector(selector) 
+}
 
+function nestedTarget()  {
+return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild()  {
+let node=document.getElementById('grand-node')
+let nextNode=node.children[0]
+while (nextNode)  {
+  node=nextNode
+  nextNode=node.children[0]
+}
+}
