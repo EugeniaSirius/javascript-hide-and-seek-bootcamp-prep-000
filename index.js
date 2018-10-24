b7fc69cec95adb78d9bb9f4b535d363f6c854146
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-24 01:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ return document.querySelector('#nested').querySelector('.target')
 
 function deepestChild()  {
 let li=document.getElementById('grand-node').querySelectorAll('div')
-//for (let i=0; i<li.length; i++)  {
-  //li[i].innerHTML = (i+1).toString()
-  //return li[i].innerHTML
-  return li[li.length-1]
-//}
+ return li[li.length-1]
 }
+
