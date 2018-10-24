bc81cf6fb191195b326b8df27a6a8177e42ea5ed
Eugenia <eugenia.kaloshina@gmail.com>
2018-10-24 01:39:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector)  {
+ return document.querySelector(selector) 
+}
 
+function nestedTarget()  {
+return document.querySelector('#nested').querySelector('.target')
+}
+
+function deepestChild()  {
+let li=document.getElementById('grand-node').querySelectorAll('div')
+ return li[li.length-1]
+}
+
+function increaseRankBy(n)  {
+  let upRank = document.getElementById('app').querySelectorAll('.ranked-list')
+  for (let i=0; i<upRank.length; i++)  {
+    upRank[i].innerHTML=parseInt((upRank[i].innerHTML)+n)
+  }
+}
